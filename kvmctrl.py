a1e5484f8f74bc6ec8d2185473d82d522ef960cb
Black September <black.september@me.com>
2014-03-15 19:48:24 +0100

    Update kvmctrl.py
    added lic

--- kvmctrl.py
+++ kvmctrl.py
@@ -2,11 +2,36 @@
 
 
 """
-Remote KVM management script.
+   Copyright (c) 2013, Black September
 
-Connects to a KVM server using SSH and executes commands that controls the KVM guest
-machines. SSH keys must be present on the server and the virsh command must be allowed to
-run as the user using sudo, but without having to type a password.
+   All rights reserved.
+ 
+   Redistribution and use in source and binary forms, with or without modification, are
+   permitted provided that the following conditions are met:
+
+   1. Redistributions of source code must retain the above copyright notice, this list
+   of conditions and the following disclaimer.
+ 
+   2. Redistributions in binary form must reproduce the above copyright notice, this
+   list of conditions and the following disclaimer in the documentation and/or other
+   materials provided with the distribution.
+ 
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND AN
+   EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+   OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+   SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+   INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+   TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+   STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+   THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+
+    Remote KVM management script.
+
+    Connects to a KVM server using SSH and executes commands that controls the KVM guest
+    machines. SSH keys must be present on the server and the virsh command must be allowed to
+    run as the user using sudo, but without having to type a password.
 """
 
 
