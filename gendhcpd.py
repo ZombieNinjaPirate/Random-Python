d70288a03e611784bd1b1b13a33067534da6ceb6
Are Hansen <black.september@me.com>
2014-07-15 20:17:20 +0200

    Update gendhcpd.py

--- gendhcpd.py
+++ gendhcpd.py
@@ -6,13 +6,8 @@
 
 #
 #   DEVELOPMENT NOTES:
-#   - assign domain name
-#   - assign DHCP range by start and end IP ||
-#     assign DHCP range by number of hosts
-#   - assign static IP's by MAC address
-#       - assign host name
-#       - verify MAC address validity
-#       - assign multiple static IP's
+#   - number ip valid ip addresses in the network
+#   - assign DHCP range by start and end IP
 #   - output in dhcpd.conf format
 #   - if user confirms configuration
 #   - write output to file
@@ -48,7 +43,7 @@
 
 __author__ = 'Are Hansen'
 __date__ = '2014, July 14'
-__version__ = '0.0.5'
+__version__ = '0.0.6'
 
 
 import os
@@ -60,30 +55,8 @@ except ImportError:
     print '\nERROR: You need the module called "ipaddr"!\n'
     sys.exit(1)
 
-"""
-authoritative;
-ddns-update-style none;
-log-facility local7;
-default-lease-time 600;
-max-lease-time 7200;
 
-option domain-name "internal";
-option domain-name-servers 8.8.8.8;
-option subnet-mask 255.255.255.0;
-option broadcast-address 10.199.115.255;
-option routers 10.199.115.1;
-
-subnet 10.199.115.0 netmask 255.255.255.0 {
-    range 10.199.115.2 10.199.115.250;
-
-    host microcloud-sshsrv028 {
-       hardware ethernet 00:11:22:33:44:55;
-       fixed-address 10.199.115.154;
-       }
-
-}
-"""
-def assign_calues():
+def assign_values():
     """Assign DHCP values. """
     print '\nNetwork configuration\n'
     while True:
@@ -102,8 +75,9 @@ def assign_calues():
     # Setup the IPv4 address for validation
     check_ips = [ipv4]
 
+    print '\nDNS Servers:'
+    print 'Multiple DNS servers as space separated list'
     dnssrv = raw_input('- Enter DNS server(s): ')
-    print '(Multiple DNS servers as space separated list)'
 
     # Append the DNS server(s) to the check list
     for dns in dnssrv.split(' '):
@@ -159,10 +133,22 @@ def assign_calues():
         # validate MAC address.
         valid_host_mac = check_mac(host_mac)
 
+        # Get host name of static host
+        valid_host_name = ''
+        while True:
+            host_name = raw_input('- Host name: ')
+
+            if len(host_name) < 3:
+                print 'The host name must be longer!'
+
+            if len(host_name) >= 3:
+                valid_host_name = host_name
+                break
+
         #
         #   CHECK FOR PREVIOUSLY USED IP AND MAC ADDRESSES
         #
-        staticip.append('{0} {1}'.format(valid_host_ip, valid_host_mac))
+        staticip.append('{0} {1} {2}'.format(valid_host_ip, valid_host_mac, valid_host_name))
 
         print staticip
         verify = raw_input('\nAdd another static IPv4 address? Y/N ')
@@ -176,7 +162,38 @@ def assign_calues():
         if verify != 'Y' and verify != 'N':
             print 'Please enter "Y" for Yes or "N" for No'
 
-    print valid_ip, cidr, dltime, mltime, dname
+    #
+    # Returned objects
+    #
+    # - valid_ip
+    #   Type: list
+    #   - 0: network
+    #   - 1 - * : DNS servers
+    #
+    # - cidr
+    #   Type: str
+    #   - CIDR of the network
+    #
+    # - dltime
+    #   Type: str
+    #   - default-lease-time
+    #
+    # - mltime
+    #   Type: str
+    #   - max-lease-time
+    #
+    # - dname
+    #   Type: str
+    #   - domain-name
+    #
+    # - staticip
+    #   Type: list
+    #   - each element contains three strings separated by a space
+    #   -- str0: ipv4 address
+    #   -- str1: mac address
+    #   -- str2: host name
+    #
+    return valid_ip, cidr, dltime, mltime, dname, staticip
 
 
 def check_mac(macadd):
@@ -184,7 +201,7 @@ def check_mac(macadd):
     while True:
         if not re.match("[0-9a-f]{2}([-:])[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", macadd.lower()):
             print 'MAC address "{0}" is not valid!'.format(macadd)
-            macadd = raw_input('- Host #{0} MAC: '.format(hostnr))
+            macadd = raw_input('- MAC: ')
 
         if re.match("[0-9a-f]{2}([-:])[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", macadd.lower()):
             break
@@ -206,10 +223,46 @@ def check_ipv4(ipv4):
     return ipv4
 
 
-def network_summary(ipcidr):
-    """Calculate gateway, netmask, network and broadcast. """
-    addr = ipcidr[0].split('.')
-    cidr = int(ipcidr[1])
+def network_summary(dhcp_info):
+    """ Processes the tuple thats returned from the assign_values function. The tuple contain six
+    elements. This is a reference of those elements:
+
+    dhcp_info[0]
+        - valid_ip
+          Type: list
+          - 0: network
+          - 1 - * : DNS servers
+
+    dhcp_info[1]
+        - cidr
+          Type: str
+          - CIDR of the network
+
+    dhcp_info[2]
+        - dltime
+          Type: str
+          - default-lease-time
+
+    dhcp_info[3]
+        - mltime
+          Type: str
+          - max-lease-time
+
+    dhcp_info[4]
+        - dname
+          Type: str
+          - domain-name
+
+    dhcp_info[5]
+        - staticip
+          Type: list
+          - each element contains three strings separated by a space
+          -- str0: ipv4 address
+          -- str1: mac address
+          -- str2: host name
+    """
+    addr = dhcp_info[0][0].split('.')
+    cidr = int(dhcp_info[1])
 
     mask = [0, 0, 0, 0]
     for i in range(cidr):
@@ -230,6 +283,18 @@ def network_summary(ipcidr):
     finalip = '{0}.{1}'.format('.'.join(map(str, broad[0:3])), int(broad[3]) - 1)
     brdcast = '{0}'.format('.'.join(map(str, broad)))
     netmask = '{0}'.format('.'.join(map(str, mask)))
+
+    print 'authoritative;'
+    print 'ddns-update-style none;'
+    print 'log-facility local7;'
+    print 'default-lease-time {0};'.format(dhcp_info[2])
+    print 'max-lease-time {0};\n'.format(dhcp_info[2])
+
+    print 'option domain-name "{0}";'.format(dhcp_info[4])
+    print 'option domain-name-servers {0};'.format(dhcp_info[1:])
+    print 'option subnet-mask {0};'.format(netmask)
+    print 'option broadcast-address {0};'.format(brdcast)
+    print 'option routers {0};'.format(brdcast)
 
     print 'Network:   {0}'.format(network)
     print 'Gateway:   {0}'.format(gateway)
@@ -255,9 +320,35 @@ def network_summary(ipcidr):
     return netsum
 
 
+"""
+authoritative;
+ddns-update-style none;
+log-facility local7;
+default-lease-time 600;
+max-lease-time 7200;
+
+option domain-name "internal";
+option domain-name-servers 8.8.8.8;
+option subnet-mask 255.255.255.0;
+option broadcast-address 10.199.115.255;
+option routers 10.199.115.1;
+
+subnet 10.199.115.0 netmask 255.255.255.0 {
+    range 10.199.115.2 10.199.115.250;
+
+    host microcloud-sshsrv028 {
+       hardware ethernet 00:11:22:33:44:55;
+       fixed-address 10.199.115.154;
+       }
+
+}
+"""
+
+
 def main():
     """...main..."""
-    assign_calues()
+    info = assign_values()
+    network_summary(info)
 
 
 if __name__ == '__main__':
